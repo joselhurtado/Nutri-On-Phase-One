f8d8b468e547cce09e4ed63fae32354a8b8d3e06
melissanohemi <melissa.castro1118@gmail.com>
2022-08-27 15:06:47 +0000

    added user info placeholders

--- src/js/component/Profile.js
+++ src/js/component/Profile.js
@@ -24,8 +24,8 @@ function Profile() {
           <div class="card" style={{ width: "18rem" }}>
             <img
               src="https://images.theconversation.com/files/478225/original/file-20220809-24-orxjcu.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
-              class="card-img-top"
-              style={{ width: "18rem", borderRadius: "10rem" }}
+              class="card-img-top mx-auto d-block"
+              style={{ width: "10rem", borderRadius: "10rem",  }}
               alt="..."
             />
             <div class="card-body">
@@ -37,25 +37,49 @@ function Profile() {
         <div class="col-8">
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+              Full Name
             </label>
             <input
-              type="email"
+              type="text"
               class="form-control"
               id="exampleFormControlInput1"
-              placeholder="name@example.com"
+              placeholder="Full Name"
             />
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">
-              Example textarea
+            <label for="exampleFormControlInput2" class="form-label">
+              Username
             </label>
-            <textarea
-              class="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-            ></textarea>
+            <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput2"
+            placeholder="Username"
+            />
           </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput3" class="form-label">
+              Email Address
+            </label>
+            <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput3"
+            placeholder="name@example.com"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput4" class="form-label">
+              Phone Number
+            </label>
+            <input
+            type="tel"
+            class="form-control"
+            id="exampleFormControlInput4"
+            placeholder="555-555-5555"
+            />
+          </div>
+        
         </div>
       </div>
     </div>
