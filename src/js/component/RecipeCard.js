7e2d1a69bd31f11d15d89d0b146662e338ac17ff
Jose Hurtado <hello@hurtadojose.com>
2022-09-03 15:47:45 +0000

    Boolean Function on Cards

--- src/js/component/RecipeCard.js
+++ src/js/component/RecipeCard.js
@@ -11,14 +11,49 @@ export default function RecipeCard() {
     }, [store.recipeData] // In Here we call out again to keep stored the data on re-load the page
     )
 
+    const vegan = () => { // Function to Work with Booleans
+        if (recipes.vegan == true) {
+            return "Yes";
+        } else {
+            return "No"
+        }
+    }
+
+    const vegetarian = () => { 
+        if (recipes.vegetarian = true) {
+            return "Yes";
+        } else {
+            return "No"
+        }
+    }
+
+    const glutenFree = () => { 
+        if (recipes.glutenFree = true) {
+            return "Yes";
+        } else {
+            return "No"
+        }
+    }
+
+    const dairyFree = () => { 
+        if (recipes.dairyFree = true) {
+            return "Yes";
+        } else {
+            return "No"
+        }
+    }
+    
+
     return (
         <div className="d-flex overflow-auto">{recipes.length > 0 && recipes.map((x,i) => 
             <div key={i} className="card m-2 cardShape" style={{minWidth: "18rem"}}>
                 <img src={`https://spoonacular.com/recipeImages/${x.id}-556x370.${x.imageType}`} className="card-img-top shapeImageTop" alt="recipe Image" />
                     <div className="card-body text-light">
                         <h4 className="card-title">{x.title}</h4>
-                        <p className="card-text">Ready in: {x.price}</p>  
-                        <p className="card-text">ID: {x.id}</p>  
+                        <p className="card-text">Vegetarian: {vegetarian()}</p>  
+                        <p className="card-text">Vegan: {vegan()}</p>  
+                        <p className="card-text">Gluten Free: {glutenFree()}</p>  
+                        <p className="card-text">Dairy Free: {dairyFree()}</p>  
                         <div className="d-flex justify-content-between">
                         <Link to={`/recipe/${x.id}`} className="btn btn-orange">
                         Read More
