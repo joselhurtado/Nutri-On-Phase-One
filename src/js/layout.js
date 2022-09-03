b85d3ceff8a3c351f3849b67037b9fb607940bc0
Jose Hurtado <hello@hurtadojose.com>
2022-09-03 16:47:09 +0000

    User Dashboard Page

--- src/js/layout.js
+++ src/js/layout.js
@@ -8,9 +8,9 @@ import injectContext from "./store/appContext";
 import { NavBar } from "./component/NavBar";
 import { Footer } from "./component/footer";
 import RecipeDetails from "/src/js/component/RecipeDetails";
+import  Profile  from "/src/js/component/Profile";
 
 
-//create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
@@ -25,11 +25,13 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
+						<Route exact path="/dashboard">
+							<Profile />
+						</Route>
 						<Route exact path="/recipe/:theid"><RecipeDetails /></Route>
 						<Route>
 							<h1 className="align-center" >Not found!</h1>
 						</Route>
-
 					</Switch>
 					<Footer />
 				</ScrollToTop>
