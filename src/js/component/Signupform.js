ea8247d6d2a9c9c109f5c6f34b9575fe30252355
JR132662 <cooljon08@gmail.com>
2022-09-17 14:13:39 +0000

    save final

--- src/js/component/Signupform.js
+++ src/js/component/Signupform.js
@@ -5,8 +5,8 @@ import "../../styles/home.css";
 export const FormUser= () => {
     return (
         <div className="form-user">
-            <input className="form-control" type="text" placeholder="Enter your username" ></input>
-            <input className="form-control" type="email" placeholder="Enter your password" ></input>
+            <input className="form-control" type="text" placeholder="Enter your First Name" required></input>
+            <input className="form-control" type="email" placeholder="Enter your Last Name" required></input>
         </div>
         );
 }
--- src/js/views/SignupQuestions.js
+++ src/js/views/SignupQuestions.js
@@ -12,12 +12,69 @@ export const SignUpQuestions = () => {
 			<div className="col-sm heroHeadline2">
 					<h1 className='text-light text-center m-2'> Tell us about yourself!</h1>
 					<div className="SignupForm2">
-						<FormUserQuestions/>
+						<div className="genderP">
+							<p className="gender-title">Select your ideal diet?</p>
+						</div>
+						<div className="dietcheckboxes">
+						<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"></input>
+								<label class="form-check-label" for="inlineCheckbox1">Vegan</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+							<label class="form-check-label" for="inlineCheckbox2">Vegetarian</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+							<label class="form-check-label" for="inlineCheckbox2">Gluten Free</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+							<label class="form-check-label" for="inlineCheckbox2">Keto</label>
+						</div>
+						<div className="2nddietcheckboxes">
+						<div class="form-check form-check-inline">
+								<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"></input>
+								<label class="form-check-label" for="inlineCheckbox1">Paleo</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+							<label class="form-check-label" for="inlineCheckbox2">Pescatarian</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+							<label class="form-check-label" for="inlineCheckbox2">No restrictions</label>
+						</div>
+						<div className="BMItitle">
+							<p className="BMIinfo">BODY MASS INDEX (BMI) IS A MEASURE OF BODY FAT BASED ON HEIGHT AND WEIGHT THAT APPLIES TO ADULT MEN AND WOMEN</p>
+						</div>
+						<div className="BMIheight">
+							<input className="feet" placeholder="Feet"></input>
+							<input className="inch" placeholder="Inches"></input>
+							<input className="weight" placeholder="Weight"></input>
+							<input className="age" placeholder="Age"></input>
+						</div>
+						<div className="BMIinfo">
+							<p className="BMIactivity">How active are you?</p>
+							<div className="BMIactivitybtn">
+								<div class="3btnweight">
+									<button class="btnactivity" type="button" id="activitybtn">Light</button>
+									<button class="btnactivity" type="button" id="activitybtn">Moderate</button>
+									<button class="btnactivity" type="button" id="activitybtn">Active</button>
+								</div>
+								<div>
+								<Link to ="/SignupQuestionsGoals">
+							<div className="SignupFormbtn">
+								<button className="btn btn-orange" id="Questionbuttons1">Go Back</button>
+								<button className="btn btn-orange" id="Questionbuttons">Continue</button>
+							</div>
+							</Link>
+								</div>
+							</div>
+						</div>
+						</div>
+						</div>
 					</div>
-					<Link to ="/SignupQuestionsGoals">
-					<div className="SignupFormbtn" id="Signupformbtn2"><button className="btn btn-primary" id="Signupbtn2">Continue</button>
-					</div>
-					</Link>
 			</div>
 		</div>
 	</div>
